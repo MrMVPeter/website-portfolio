a11ad91cb3fb78ec9703f3d285c9d5d5d0d286c8
Peter Gilliam <gilliampeter@gmail.com>
2022-11-26 18:05:32 -0500

    na

--- src/components/Home/textItems.js
+++ src/components/Home/textItems.js
@@ -3,7 +3,7 @@ const homeText = {
   textHeader: ["Hello! My Name is ", "Peter!"],
   textSubHeader: "A Software Developer",
   textBody:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu ac tortor dignissim convallis aenean et tortor. Placerat vestibulum lectus mauris ultrices eros in cursus turpis massa. Id volutpat lacus laoreet non curabitur gravida arcu ac. Accumsan tortor posuere ac ut. Fusce ut placerat orci nulla pellentesque dignissim enim sit. Est lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque. Feugiat vivamus at augue eget arcu dictum varius duis. Nisi scelerisque eu ultrices vitae auctor. Enim sed faucibus turpis in eu. Facilisis volutpat est velit egestas dui id ornare arcu odio.",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu ac tortor dignissim convallis aenean et tortor. Placerat vestibulum lectus mauris ultrices eros in cursus turpis massa. Id volutpat lacus laoreet non curabitur gravida cing odio.",
 };
 
 export default homeText;
