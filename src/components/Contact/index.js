0983e70921fe22d62643282778aa07e78da42da7
Peter Gilliam <gilliampeter@gmail.com>
2022-12-18 23:29:16 -0500

    Added An imbedded Map

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -6,6 +6,16 @@ function Contact() {
     <body className="BodyPage">
       <div className="TEMP1">
         <h1>Contact</h1>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11701.650342329605!2d-82.90026977344345!3d42.84302064650424!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8824fd9a01f3a803%3A0xfcfeceb42e5ae313!2sArmada%2C%20MI%2048005!5e0!3m2!1sen!2sus!4v1671421117998!5m2!1sen!2sus"
+          width="600"
+          height="450"
+          style={{ width: 90 + "vw" }}
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+          title="Google Maps Embeded"
+        ></iframe>
       </div>
     </body>
   );
--- src/components/Home/textItems.js
+++ src/components/Home/textItems.js
@@ -1,7 +1,7 @@
 // This Script Lays out the Home page text contents
 const homeText = {
   textHeader: ["Hello! My Name is ", "Peter!"],
-  textSubHeader: "A Systems Administrator",
+  textSubHeader: "A Manufacturing Systems Administrator",
   textBody:
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu ac tortor dignissim convallis aenean et tortor. Placerat vestibulum lectus mauris ultrices eros in cursus turpis massa. Id volutpat lacus laoreet non curabitur gravida arcu ac. Accumsan tortor posuere ac ut. Fusce ut placerat orci nulla pellentesque dignissim enim sit. Est lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque. Feugiat vivamus at augue eget arcu dictum varius duis. Nisi scelerisque eu ultrices vitae auctor. Enim sed faucibus turpis in eu. Facilisis volutpat est velit egestas dui id ornare arcu odio.",
 };
