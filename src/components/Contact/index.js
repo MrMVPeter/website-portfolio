41f96d55ac90b85f093b9ddd05b1273b6144beb4
Peter Gilliam <gilliampeter@gmail.com>
2022-12-19 23:28:18 -0500

    Added Map Parent DIV

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,13 +4,17 @@ import "./index.scss";
 function Contact() {
   return (
     <body className="BodyPage">
-      <div className="TEMP1">
+      <div className="Contact">
         <h1>Contact</h1>
+        <h1>peter.n.gilliam@gmail.com</h1>
+      </div>
+      <div id="googleMapsContainer">
         <iframe
+          id="googleMaps"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11701.650342329605!2d-82.90026977344345!3d42.84302064650424!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8824fd9a01f3a803%3A0xfcfeceb42e5ae313!2sArmada%2C%20MI%2048005!5e0!3m2!1sen!2sus!4v1671421117998!5m2!1sen!2sus"
           width="600"
           height="450"
-          style={{ width: 90 + "vw" }}
+          // style={{}}
           allowfullscreen=""
           loading="lazy"
           referrerpolicy="no-referrer-when-downgrade"
