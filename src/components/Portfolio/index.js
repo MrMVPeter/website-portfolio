a67be3950d02bce7d781b7e749ba27a20d9c870c
Peter Gilliam <gilliampeter@gmail.com>
2023-03-30 23:02:41 -0400

    Fixed UI bug

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,10 +1,6 @@
 import React from "react";
 import "./index.scss";
-<<<<<<< HEAD
 // import projectComponent from "./projectComponent";
-=======
-import projectData from "./projectData";
->>>>>>> main
 
 function ProjectTile(props) {
   return (
@@ -19,16 +15,8 @@ function Portfolio() {
   return (
     <body className="bodyPage" id="portfolioPage">
       <h1>Portfolio Page</h1>
-<<<<<<< HEAD
       {/* <projectComponent></projectComponent> */}
       {/* <Portfolio></Portfolio> */}
-=======
-      <div id="project_list">
-        {projectData.map((item, index) => {
-          return <ProjectTile />;
-        })}
-      </div>
->>>>>>> main
     </body>
   );
 }
