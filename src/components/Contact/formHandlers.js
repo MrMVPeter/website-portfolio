6e2ea7da729ac79e7385ab5f0e66c9b2baa64e87
Peter Gilliam <gilliampeter@gmail.com>
2023-04-03 01:43:14 -0400

    update to test POST

--- src/components/Contact/formHandlers.js
+++ src/components/Contact/formHandlers.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
-async function handleSubmit(event) {
-  event.preventDefault();
-  const name = event.target.name.value;
-  const email = event.target.email.value;
-  const message = event.target.message.value;
+async function handleSubmit(formData) {
+  const { name, email, message } = formData;
 
   try {
     const response = await axios.post("/.netlify/functions/submitForm", {
@@ -16,7 +13,6 @@ async function handleSubmit(event) {
     if (response.data.status === "success") {
       // Handle success e-g, show a success message or clear the form
       alert("Your message has been sent successfully.");
-      event.target.reset();
     } else {
       // Handle error e-g, show an error message
       alert("An error occurred. Please try again later.");
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -19,6 +19,7 @@ function Contact() {
   // Turns handle submit function into an event handler
   const submitForm = (event) => {
     event.preventDefault();
+    console.log(formData);
     handleSubmit(formData);
   };
 
@@ -30,8 +31,8 @@ function Contact() {
         <form id="ContactBody" onSubmit={submitForm}>
           <label>Full Name:</label>
           <input
-            name="name"
-            value={formData.fullname}
+            name="fullName"
+            value={formData.fullName}
             onChange={handleChange}
           />
           <label>Return Email Address:</label>
@@ -57,7 +58,6 @@ function Contact() {
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11701.650342329605!2d-82.90026977344345!3d42.84302064650424!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8824fd9a01f3a803%3A0xfcfeceb42e5ae313!2sArmada%2C%20MI%2048005!5e0!3m2!1sen!2sus!4v1671421117998!5m2!1sen!2sus"
         width="600"
         height="450"
-        // style={{}=>()}
         allowfullscreen=""
         loading="lazy"
         referrerpolicy="no-referrer-when-downgrade"
