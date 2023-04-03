3cad900f38a3b4c10bab54854bda214478c6cb82
Peter Gilliam <gilliampeter@gmail.com>
2023-04-03 02:43:59 -0400

    fix the from location

--- functions/submitForm.js
+++ functions/submitForm.js
@@ -1,8 +1,6 @@
 const nodemailer = require("nodemailer");
 
 exports.handler = async function(event, context) {
-  console.log("Function started");
-
   if (event.httpMethod !== "POST") {
     return {
       statusCode: 405,
@@ -10,7 +8,7 @@ exports.handler = async function(event, context) {
     };
   }
 
-  const { name, email, message } = JSON.parse(event.body);
+  const { fullName, email, message } = JSON.parse(event.body);
 
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
@@ -23,23 +21,19 @@ exports.handler = async function(event, context) {
   });
 
   const mailOptions = {
-    from: `${name} <${email}>`,
+    from: `${fullName} <${email}>`,
     to: process.env.EMAIL_TO,
     subject: "New message from your portfolio website",
     text: message,
   };
 
-  console.log("About to send email"); // Add this line
-
   try {
     await transporter.sendMail(mailOptions);
-    console.log("Email sent successfully"); // Add this line
     return {
       statusCode: 200,
       body: JSON.stringify({ status: "success", message: "Email Sent" }),
     };
   } catch (error) {
-    console.error("Email sending failed:", error); // Add this line
     return {
       statusCode: 500,
       body: JSON.stringify({
