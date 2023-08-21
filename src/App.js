de0121845513eac39d3569b86739ee41793716cb
Peter Gilliam <gilliampeter@gmail.com>
2023-08-21 15:52:46 -0400

    Added og:image tag for SEO

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import "./App.scss";
 import NavBar from "./components/NavBar";
 import FooterBar from "./components/FooterBar";
 import ReactRouterRoutes from "./ReactRouterRoutes";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <div id="App">
+      <Helmet>
+        <title>Peter Gilliam's Portfolio</title>
+        <meta name="" content="" />
+      </Helmet>
       {/* Navigation Bar Compoment */}
       <NavBar />
 
